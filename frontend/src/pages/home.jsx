d5047d551bc3f85874ee00a852e3d5c61c04da11
glory <gloryrp58@gmail.com>
2025-11-13 16:09:46 +0530

    Deploying Frontend

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -75,7 +75,7 @@ function HomeComponent() {
 
             <div className="navBar">
                 <div style={{ display: "flex", alignItems: "center" }}>
-                    <h2>Apna Video Call</h2>
+                    <h2>EchoLink</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
